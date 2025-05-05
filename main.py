56f3ff8f109e8af50c8407f38aa859d458a9551d
Dagimmengestu5 <dagimmengestu5@gmail.com>
2025-05-05 16:17:40 +0300

    home work

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import json
 
 
 # === Config ====
-TOKEN = "7945188969:AAGqv31lZK0YaRjVTDqBXgTiCJyt1hyICnc"  # Telegram token from environment variable
+TOKEN = "7945188969:AAHeIHDGvAi0EZAZCjtz_w1TuwKbqkYvz2Q"  # Telegram token from environment variable
 GOOGLE_CREDENTIALS_PATH = os.getenv("google_key.json")  # Google credentials from environment variable
 ETHIOPIA_TZ = pytz.timezone("Africa/Addis_Ababa")
 BOT_PASSWORD = ["dagi","Dagi","droga"]
